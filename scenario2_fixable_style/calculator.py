51cc7b7c094f9f62d2ab7e3560024c3187f641ec
PranavKuppa <sspkuppa@gmail.com>
2026-06-21 23:42:21 +0530

    style: automated formatting pass [bot]

--- scenario2_fixable_style/calculator.py
+++ scenario2_fixable_style/calculator.py
@@ -1,15 +1,21 @@
-'''Calculator module with style violations.'''
+"""Calculator module with style violations."""
+
 import os
 import sys
-def add(a,b):
-    x=a+b
+
+
+def add(a, b):
+    x = a + b
     return x
 
-def subtract(a, b):
-        return a-b
 
-class Calculator :
-    def __init__(self,name):
-        self.name=name
-    def multiply(self,a,b):
-        return a*b
+def subtract(a, b):
+    return a - b
+
+
+class Calculator:
+    def __init__(self, name):
+        self.name = name
+
+    def multiply(self, a, b):
+        return a * b
