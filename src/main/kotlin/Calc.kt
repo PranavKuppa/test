811ce57b7f2e9acd7320422d98e361351b931797
d <d@e.com>
2026-06-22 18:16:16 +0530

    style: automated formatting pass [bot]

--- src/main/kotlin/Calc.kt
+++ src/main/kotlin/Calc.kt
@@ -1,8 +1,12 @@
-fun add(a:Int,b:Int):Int{
-val result=a+b
-return result
+fun add(
+    a: Int,
+    b: Int,
+): Int {
+    val result = a + b
+    return result
 }
-fun subtract(a: Int, b: Int): Int
-{
-    return a-b
-}
+
+fun subtract(
+    a: Int,
+    b: Int,
+): Int = a - b
