0bc151079be2e7c3f63b09ab01a12b36d662f4ee
PranavKuppa <sspkuppa@gmail.com>
2026-07-06 01:38:15 +0530

    New file

---
+++ test.py
@@ -0,0 +1,15 @@
+def add(a,b):
+    x = a+ b
+    return x
+
+
+def subtract(a, b):
+    return a -b
+
+
+class Calculator:
+    def __init__(self, name):
+        self.name=name
+
+    def multiply(self, a, b):
+        return a*  b
