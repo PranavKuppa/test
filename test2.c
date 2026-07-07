851dea9bdbd4df5a561baeb9b67340e2d909b4c5
PranavKuppa <85881002+PranavKuppa@users.noreply.github.com>
2026-07-07 22:14:15 +0530

    test2

---
+++ test2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include<math.h>
+
+int main() {
+
+
+  
+int number;
+  
+  printf("Enter an integer: " );
+    scanf("%d", &number );
+ if (number < 0) {
+printf("You entered a negative number.\n");
+       }
+         else {
+   printf("You entered a positive number.\n");
+ }
+return 0 ;
+}
