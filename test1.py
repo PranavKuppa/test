169273e49e93ef26e40f3db75a7376dcb779a53a
PranavKuppa <85881002+PranavKuppa@users.noreply.github.com>
2026-07-07 22:07:20 +0530

    Refactor Calculator and update test cases
    Refactor Calculator class and test functions.

--- test1.py
+++ test1.py
@@ -13,16 +13,6 @@ def subtract(a, b):
 
 
 class Calculator:
-    def __init__(self, name):def add(a, b):
-    x = a + b
-    return x
-
-
-def subtract(a, b):
-    return a - b
-
-
-class Calculator:
     def __init__(self, name):
         self.name = name
 
@@ -31,23 +21,6 @@ class Calculator:
 
 
 def test_add():
-    assert add(2, 3) == 5
-
-
-def test_subtract():
-    assert subtract(5, 2) == 3
-
-
-def test_multiply():
-    calc = Calculator("test")
-    assert calc.multiply(3, 4) == 12
-        self.name =      name
-
-    def multiply(self, a,    b):
-        return a *b
-
-
-def test_add(  ):
     assert add(2,3) == 5
 
 
