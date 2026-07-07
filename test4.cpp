23d84bc7f4b7dfaeb0d8a62caa915ec16e727ea1
PranavKuppa <85881002+PranavKuppa@users.noreply.github.com>
2026-07-07 22:16:21 +0530

    test4

---
+++ test4.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include<iomanip.h>
+#include<math.h>
+#include<conio.h>
+
+
+
+using namespace std;
+
+int main() {
+
+  
+    int count = 10;
+  for(int i=0 ; i<count; i++) {
+cout << "Iteration: " << i << endl ; 
+      if(i ==5) {
+    cout << "Halfway there!" << endl;
+         }
+  }
+    return 0;
+}
